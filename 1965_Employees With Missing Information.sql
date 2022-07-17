8853d7a99e7c058b0238a1932b598e9de2c0932f
tina0420 <hsu.tina88@yahoo.com.tw>
2022-07-17 12:13:57 +0800

    1965.Employees With Missing Information

---
+++ 1965_Employees With Missing Information.sql
@@ -0,0 +1,69 @@
+/*
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+
+Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+Example:
+
+Input: 
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation: 
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing.
+*/
+
+select new.employee_id from 
+((select Employees.employee_id from Employees left join Salaries on Employees.employee_id = Salaries.employee_id where Salaries.employee_id is null)
+ 
+ union
+ 
+ (select Salaries.employee_id from Salaries left join Employees on Employees.employee_id = Salaries.employee_id where Employees.employee_id is null))
+
+as new order by employee_id
+
