fefa0446d2248b355f11386d0f0c37ff2096214e
tina0420 <hsu.tina88@yahoo.com.tw>
2022-07-17 12:05:12 +0800

    1667.Fix Names in a Table

---
+++ 1667_Fix Names in a Table.sql
@@ -0,0 +1,44 @@
+/*
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+Return the result table ordered by user_id.
+
+Example:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+*/
+
+-- Method1-Oracle:
+
+select user_id, initcap(name) as name from Users order by user_id
+
+
+-- Method2-MySQL
+
+select user_id, concat(upper(left(name,1)), lower(substr(name,2,length(name)-1))) as name from Users order by user_id
+
