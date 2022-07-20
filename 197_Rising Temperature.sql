ddf56614f3bf0caf6dc119494ad3804f0d964a29
tina0420 <hsu.tina88@yahoo.com.tw>
2022-07-20 15:38:10 +0800

    197.Rising Temperature

---
+++ 197_Rising Temperature.sql
@@ -0,0 +1,43 @@
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+Return the result table in any order.
+
+Example:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+*/
+
+select w_after.id from Weather w_before, Weather w_after where w_after.temperature > w_before.temperature and subdate(w_after.recordDate,1) = w_before.recordDate
+
