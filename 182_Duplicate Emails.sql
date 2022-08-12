147db1e39f221274a6257ffab6d01f777308bdaa
tina0420 <hsu.tina88@yahoo.com.tw>
2022-08-12 14:25:43 +0800

    182.Duplicate Emails

---
+++ 182_Duplicate Emails.sql
@@ -0,0 +1,37 @@
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+Write an SQL query to report all the duplicate emails.
+Return the result table in any order.
+
+Example:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+*/
+
+select distinct p1.email as Email from Person p1, Person p2 where p1.email=p2.email group by email having count(*) > 1
